e560707180924303148f34c7b7a29bd9afc98b54
tkawahara-inrevo <t.kawahara@inrevo.jp>
2026-05-15 12:16:50 +0900

    initial commit

---
+++ api/check.js
@@ -0,0 +1,22 @@
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyr6gKWiH3_hZ629JnUE93u3ZX59aBrdUiHpt5s_1CZdwa3feHXbhlRWdIWykEd9m0f/exec';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  try {
+    const body = req.body || {};
+    const params = new URLSearchParams(body).toString();
+    const response = await fetch(GAS_URL + '?' + params, { redirect: 'follow' });
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(200).json({ error: err.message });
+  }
+}
